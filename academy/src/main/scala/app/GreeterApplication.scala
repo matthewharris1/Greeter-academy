93e1adcff8f64521d25bedd74dc9e112ce1d3428
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-01-22 11:19:13 +0000

    Created an if statement using the StdIn import and string interpolation

--- academy/src/main/scala/app/GreeterApplication.scala
+++ academy/src/main/scala/app/GreeterApplication.scala
@@ -1,8 +1,20 @@
 package app
 
+import scala.io.StdIn
 /**
   * Created by digital032676 on 22/01/18.
   */
 object GreeterApplication extends App {
- println("Hello!")
+
+ def greet(name: String): Unit = {
+   if (name == "Matthew") {
+     println("Hello Matthew")
+    } else {
+     println(s"Hello $name")
+   }
+ }
+
+ val name = StdIn.readLine("What is your name? ")
+
+  greet(name)
 }
