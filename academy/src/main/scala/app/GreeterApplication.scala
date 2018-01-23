8e0e4c96561e920b7516396406333188a4edd0c0
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-01-23 12:02:20 +0000

    Added Person Class and Prompt Object

--- academy/src/main/scala/app/GreeterApplication.scala
+++ academy/src/main/scala/app/GreeterApplication.scala
@@ -4,18 +4,35 @@ import scala.io.StdIn
 /**
   * Created by digital032676 on 22/01/18.
   */
+
+class Person(name: String, age: Int) {
+
+  def greet() : Unit = {
+    if (name == "Matthew") {
+      println(s"Hello Matthew, you are $age years old")
+    } else {
+      println(s"Hello $name I don't know how old you are")
+    }
+  }
+}
+
+object Prompt {
+
+  def name(message: String) : String= StdIn.readLine(message)
+
+  def age() = StdIn.readLine().toInt
+
+}
 object GreeterApplication extends App {
 
- def greet(name: String, age: Int): Unit = {
-   if (name == "Matthew") {
-     println(s"Hello Matthew, you are $age years old")
-    } else {
-     println(s"Hello $name I don't know how old you are")
-   }
- }
+    val name = {
+      Prompt.name("What is your name? ")
+      Prompt.age().toString
+    }
 
- val name = StdIn.readLine("What is your name? ")
-  val age = 21
 
-  greet(name, age)
+    val Matt = new Person(name, 21)
+
+    println(Matt.greet())
+
 }
